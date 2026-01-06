7b80a3312b1ba2a452a51d01d88e7e1432c27f4c
Valentin Sigel <74764910+ValentinSi@users.noreply.github.com>
2026-01-06 16:42:55 +0100

    adding multi-file pointcloud and html with potree

---
+++ pointclouds/BA_Untersuchungsgebiet_subset/cloud.js
@@ -0,0 +1,99 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 25466615,
+    "boundingBox": {
+        "lx": 417951.997,
+        "ly": 5353281.206,
+        "lz": 108.057,
+        "ux": 418439.042,
+        "uy": 5353768.251,
+        "uz": 595.1020000000419
+    },
+    "tightBoundingBox": {
+        "lx": 417951.997,
+        "ly": 5353281.206,
+        "lz": 108.057,
+        "ux": 418439.042,
+        "uy": 5353599.499,
+        "uz": 259.928
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "normal x",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        },
+        {
+            "name": "normal y",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        },
+        {
+            "name": "normal z",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        }
+    ],
+    "spacing": 4.217933654785156,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
